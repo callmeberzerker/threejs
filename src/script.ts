106ae52768f2e44e0ab2d145dd9a869985af6a26
callmeberzerker <smarkovski@gmail.com>
2024-08-24 12:43:08 +0200

    changes

--- src/script.ts
+++ src/script.ts
@@ -1,55 +1,61 @@
-import * as THREE from "three";
+import * as THREE from 'three'
 
 // Canvas
-const canvas = document.querySelector("canvas.webgl");
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
 /**
  * Object
  */
 
 const mesh = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
 
-scene.add(mesh);
+scene.add(mesh)
 
 /**
  * Sizes
  */
 const sizes = {
-  width: 800,
-  height: 600,
-};
+    width: 800,
+    height: 600,
+}
 
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 
-camera.position.set(0, 0, 3);
-scene.add(camera);
+camera.position.set(0, 0, 3)
+scene.add(camera)
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas!,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+    canvas: canvas!,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+
+let time = Date.now()
 
 const tick = () => {
-  // Call tick again on the next frame
+    // Call tick again on the next frame
+    const currentTime = Date.now()
+    const deltaTime = currentTime - time
+    console.log(deltaTime)
+    time = currentTime
 
-  mesh.position.x -= 0.01;
-  mesh.position.x -= 0.01;
+    mesh.rotation.y += 0.01
+    // mesh.position.x -= 0.01;
 
-  renderer.render(scene, camera);
-  window.requestAnimationFrame(tick);
-};
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
 
-tick();
+tick()
